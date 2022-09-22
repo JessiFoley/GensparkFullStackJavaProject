e7607d7891cf3d824c7cb68afccfa28f301f1012
CBush88 <christopherbush88@gmail.com>
2022-09-22 17:26:09 -0400

    Removed useEffect form App.js (still works) fixed button in App.css

--- JavaFullStackBookstore/frontend/src/App.js
+++ JavaFullStackBookstore/frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { React, useEffect, useState, } from "react";
+import { React, useState, } from "react";
 import { retrieveAllBooks } from "./service/BooksDataService";
 import BookInventory from './components/books/BookInventory';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AddBook from './components/books/AddBook';
 import UpdateBook from './components/books/UpdateBook';
+import Header from './components/Header';
+import Footer from './components/Footer'
 
 const App = () => {
   const [books, setBooks] = useState([]);
@@ -18,19 +20,17 @@ const App = () => {
       .catch(err => console.log(err.response));
   }
 
-  useEffect(() => {
-    getBooks();
-  }, []);
-
   return (
     <Router>
       <div className="App">
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<BookInventory books={books} getBooks={getBooks} setBooks={setBooks} setBookToUpdate={setBookToUpdate} />} />
             <Route path="/add" element={<AddBook />} />
             <Route path="/update" element={<UpdateBook bookToUpdate={bookToUpdate} />} />
           </Routes>
+          <Footer />
         </div>
       </div>
     </Router>
