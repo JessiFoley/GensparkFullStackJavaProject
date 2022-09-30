a83f33dad29589503e1bc0011c558ac332c50f8c
CBush88 <christopherbush88@gmail.com>
2022-09-29 20:01:52 -0400

    Reverted amplify

--- JavaFullStackBookstore/frontend/src/App.js
+++ JavaFullStackBookstore/frontend/src/App.js
@@ -2,38 +2,27 @@ import './App.css';
 import { React, useState, } from "react";
 import { retrieveAllBooks } from "./service/BooksDataService";
 import BookInventory from './components/books/BookInventory';
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom"
 import AddBook from './components/books/AddBook';
 import UpdateBook from './components/books/UpdateBook';
 import Header from './components/Header';
-import Footer from './components/Footer';
-// Commented code is for non-amazon authentication
-// import LoginForm from './components/login/LoginForm';
-// import { retrieveAllUsers } from './service/UsersDataService';
-// import SignupForm from './components/login/SignupForm';
-// import bcrypt from "bcryptjs";
-import "@aws-amplify/ui-react/styles.css";
-import{
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import Footer from './components/Footer'
+import LoginForm from './components/login/LoginForm';
+import { retrieveAllUsers } from './service/UsersDataService';
+import SignupForm from './components/login/SignupForm';
+import bcrypt from "bcryptjs";
 
-
-const App = ({ signOut }) => {
+const App = () => {
   const [books, setBooks] = useState([]);
   const [bookToUpdate, setBookToUpdate] = useState({});
-  // const [users, setUsers] = useState([]);
-  // const [loggedIn, setLoggedIn] = useState(null);
-  // const [userAttempt, setUserAttempt] = useState({
-  //   "username": "",
-  //   "password": "",
-  // });
+  const [users, setUsers] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(null);
+  const [userAttempt, setUserAttempt] = useState({
+    "username": "",
+    "password": "",
+  });
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   let getBooks = () => {
     retrieveAllBooks()
@@ -43,30 +32,42 @@ const App = ({ signOut }) => {
       .catch(err => console.log(err.response));
   }
 
-  // const getUsers = () => {
-  //   retrieveAllUsers()
-  //     .then(res => {
-  //       setUsers(res.data);
-  //     })
-  //     .catch(err => console.log(err.response));
-  // }
+  const getUsers = () => {
+    retrieveAllUsers()
+      .then(res => {
+        setUsers(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
 
-  // const authenticate = async () => {
-  //   users.forEach(user => {
-  //     if (user.username === userAttempt.username) {
-  //       if (bcrypt.compareSync(userAttempt.password, user.password)) {
-  //         setLoggedIn(userAttempt?.username);
-  //         navigate("/");
-  //       }
-  //     }
-  //   });
-  // }
+  const authenticate = async () => {
+    users.forEach(user => {
+      if (user.username === userAttempt.username) {
+        if (bcrypt.compareSync(userAttempt.password, user.password)) {
+          setLoggedIn(userAttempt?.username);
+          navigate("/");
+        }
+      }
+    });
+  }
+
+  if (!loggedIn) {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<LoginForm getUsers={getUsers} setUserAttempt={setUserAttempt} userAttempt={userAttempt} authenticate={authenticate} />} />
+            <Route path="/signup" element={<SignupForm getUsers={getUsers} />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
+    )
+  }
 
   return (
-    <View className="App">
-      <Card>
-        {/* <Heading level={1}>We now have Auth!</Heading> */}
-      </Card>
+    <div className="App">
       <div className="container">
         <Header />
         <Routes>
@@ -76,42 +77,8 @@ const App = ({ signOut }) => {
         </Routes>
         <Footer />
       </div>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    </div>
   );
 }
 
-export default withAuthenticator(App);
-
-  // Working without AWS below
-  // if (!loggedIn) {
-  //   return (
-  //     <div className="App">
-  //       <div className="container">
-  //         <Header />
-  //         <Routes>
-  //           <Route path="/" element={<LoginForm getUsers={getUsers} setUserAttempt={setUserAttempt} userAttempt={userAttempt} authenticate={authenticate} />} />
-  //           <Route path="/signup" element={<SignupForm getUsers={getUsers} />} />
-  //         </Routes>
-  //         <Footer />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-
-//   return (
-//     <div className="App">
-      // <div className="container">
-      //   <Header />
-      //   <Routes>
-      //     <Route exact path="/" element={<BookInventory books={books} getBooks={getBooks} setBooks={setBooks} setBookToUpdate={setBookToUpdate} />} />
-      //     <Route path="/add" element={<AddBook />} />
-      //     <Route path="/update" element={<UpdateBook bookToUpdate={bookToUpdate} />} />
-      //   </Routes>
-      //   <Footer />
-      // </div>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
