59fb8ef1bccd898182777f5570953df27ad7f55f
Jessi Foley <jessifreyberger@gmail.com>
2022-10-04 12:16:10 -0500

    added icon and navbar

---
+++ JavaFullStackBookstore/frontend/src/components/Nav.js
@@ -0,0 +1,29 @@
+
+const Nav = () => {
+    return (
+  
+      <nav class="navbar navbar-expand-sm bg-light navbar-light">
+  
+    <a class="navbar-brand" href="#">Bookstore Inventory</a>
+    
+  
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/signup/">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    </ul>
+  </nav>
+  
+          )
+  }
+  
+  export default Nav
