98f7e342fb5e6e4a88685ed4d14002e6f32f981c
Jessi Foley <jessifreyberger@gmail.com>
2022-10-04 12:16:10 -0500

    added icon and navbar

--- JavaFullStackBookstore/frontend/src/components/Header.js
+++ JavaFullStackBookstore/frontend/src/components/Header.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import Nav from './Nav'
 
 const Header = () => {
   return (
     <div>
-        <h3>Welcome to Bookstore!</h3>
-        <nav navbar with form>
-          <ul className='nav'>
-            <li className='nav-item'><Link className='nav-link' to="/">Home</Link></li>
-            <li className='nav-item'><Link className='nav-link' to="/">Book List</Link></li>
-          </ul>
-        </nav>
+        <Nav />
     </div>
   )
 }
